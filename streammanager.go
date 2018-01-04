17c90b9cd687dcf03eeef4622335c414edb3be17
Federico Maggi <federico.maggi@xtn-lab.com>
2018-01-04 18:22:16 +0100

    [UPD] Refactored stream manager

--- streammanager.go
+++ streammanager.go
@@ -6,44 +6,33 @@ import (
 
 // StreamManager wraps up all supported stream types.
 type StreamManager struct {
-	debugStreams []Stream
-	verboseStreams []Stream
-	infoStreams []Stream
-	warningStreams []Stream
-	errorStreams []Stream
-	fatalStreams []Stream
+	streams map[LogLevel][]Stream
 }
 
 // NewStreamManager creates a new, properly initialised, StreamManager instance.
 func NewStreamManager() *StreamManager {
-	return &StreamManager{
-		debugStreams: make([]Stream,0),
-		verboseStreams: make([]Stream,0),
-		infoStreams: make([]Stream,0),
-		warningStreams: make([]Stream,0),
-		errorStreams: make([]Stream,0),
-		fatalStreams: make([]Stream,0),
-	}
+	s := &StreamManager{}
+	s.streams = make(map[LogLevel][]Stream)
+	s.streams[Debug] = make([]Stream, 0)
+	s.streams[Verbose] = make([]Stream, 0)
+	s.streams[Info] = make([]Stream, 0)
+	s.streams[Warning] = make([]Stream, 0)
+	s.streams[Error] = make([]Stream, 0)
+	s.streams[Fatal] = make([]Stream, 0)
+	s.streams[Panic] = make([]Stream, 0)
+
+	return s
 }
 
 // Register internally saves provided stream into proper container.
 func (s *StreamManager) Register(level LogLevel, stream Stream) error {
-	switch level {
-	case Debug:
-		s.debugStreams = append(s.debugStreams, stream)
-	case Verbose:
-		s.verboseStreams = append(s.verboseStreams, stream)
-	case Info:
-		s.infoStreams = append(s.infoStreams, stream)
-	case Warning:
-		s.warningStreams = append(s.warningStreams, stream)
-	case Error:
-		s.errorStreams = append(s.errorStreams, stream)
-	case Fatal:
-		s.fatalStreams = append(s.fatalStreams, stream)
-	default: 
+	registeredStreams, ok := s.streams[level]
+	if !ok {
 		return fmt.Errorf("invalid log level provided")
 	}
+
+	registeredStreams = append(registeredStreams, stream)
+	s.streams[level] = registeredStreams
 	return nil
 }
 
@@ -52,76 +41,19 @@ func (s *StreamManager) Send(level LogLevel, message *LogMessage) error {
 	if message == nil {
 		return fmt.Errorf("invalid nil message")
 	}
-	
+
+	registeredStreams, ok := s.streams[level]
+	if !ok {
+		return fmt.Errorf("invalid log level provided")
+	}
+
 	messageString, err := message.Serialise()
 	if err != nil {
 		return fmt.Errorf("serialisation error: %s", err.Error())
 	}
 
-	switch level {
-	case Debug:
-		s.sendToDebugStreams(messageString)
-	case Verbose:
-		s.sendToVerboseStreams(messageString)
-	case Info:
-		s.sendToInfoStreams(messageString)
-	case Warning:
-		s.sendToWarningStreams(messageString)
-	case Error:
-		s.sendToErrorStreams(messageString)
-	case Fatal:
-		s.sendToFatalStreams(messageString)
-
-	default: 
-		return fmt.Errorf("invalid log level provided")
-	}
-	return nil
-}
-
-// sendToDebugStreams fires writes for all registered Debug streams.
-func (s *StreamManager) sendToDebugStreams(message string) error {
-	for i := 0; i < len(s.debugStreams); i++ {
-		s.debugStreams[i].Write(message)
-	}
-	return nil
-}
-
-// sendToVerboseStreams fires writes for all registered Verbose streams.
-func (s *StreamManager) sendToVerboseStreams(message string) error {
-	for i := 0; i < len(s.verboseStreams); i++ {
-		s.verboseStreams[i].Write(message)
-	}
-	return nil
-}
-
-// sendToInfoStreams fires writes for all registered Info streams.
-func (s *StreamManager) sendToInfoStreams(message string) error {
-	for i := 0; i < len(s.infoStreams); i++ {
-		s.infoStreams[i].Write(message)
-	}
-	return nil
-}
-
-// sendToWarningStreams fires writes for all registered Warning streams.
-func (s *StreamManager) sendToWarningStreams(message string) error {
-	for i := 0; i < len(s.warningStreams); i++ {
-		s.warningStreams[i].Write(message)
-	}
-	return nil
-}
-
-// sendToErrorStreams fires writes for all registered Error streams.
-func (s *StreamManager) sendToErrorStreams(message string) error {
-	for i := 0; i < len(s.errorStreams); i++ {
-		s.errorStreams[i].Write(message)
-	}
-	return nil
-}
-
-// sendToFatalStreams fires writes for all registered Fatal streams.
-func (s *StreamManager) sendToFatalStreams(message string) error {
-	for i := 0; i < len(s.fatalStreams); i++ {
-		(s.fatalStreams[i]).Write(message)
+	for i := 0; i < len(registeredStreams); i++ {
+		registeredStreams[i].Write(messageString)
 	}
 	return nil
 }
--- streammanager_test.go
+++ streammanager_test.go
@@ -13,137 +13,179 @@ func TestStreamManagerRegister(t *testing.T) {
 		t.Fatalf("Expected error for invalid loglevel. Found nil instead.")
 	}
 
-
 	if err := manager.Register(Debug, stream); err != nil {
 		t.Fatalf("Unexpected error: %s", err.Error())
 	}
-	if len(manager.debugStreams) != 1 {
-		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.debugStreams))
+	if len(manager.streams[Debug]) != 1 {
+		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Debug]))
 	}
-	if len(manager.verboseStreams) != 0 {
-		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 0, len(manager.verboseStreams))
+	if len(manager.streams[Verbose]) != 0 {
+		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Verbose]))
 	}
-	if len(manager.infoStreams) != 0 {
-		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 0, len(manager.infoStreams))
+	if len(manager.streams[Info]) != 0 {
+		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Info]))
 	}
-	if len(manager.warningStreams) != 0 {
-		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 0, len(manager.warningStreams))
+	if len(manager.streams[Warning]) != 0 {
+		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Warning]))
 	}
-	if len(manager.errorStreams) != 0 {
-		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 0, len(manager.errorStreams))
+	if len(manager.streams[Error]) != 0 {
+		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Error]))
 	}
-	if len(manager.fatalStreams) != 0 {
-		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.warningStreams))
+	if len(manager.streams[Fatal]) != 0 {
+		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Fatal]))
+	}
+	if len(manager.streams[Panic]) != 0 {
+		t.Fatalf("Unexpected panic stream len. Expected: %d - Found: %d.", 1, len(manager.streams[Panic]))
 	}
 
 	if err := manager.Register(Verbose, stream); err != nil {
 		t.Fatalf("Unexpected error: %s", err.Error())
 	}
-	if len(manager.debugStreams) != 1 {
-		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.debugStreams))
+	if len(manager.streams[Debug]) != 1 {
+		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Debug]))
 	}
-	if len(manager.verboseStreams) != 1 {
-		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.verboseStreams))
+	if len(manager.streams[Verbose]) != 1 {
+		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Verbose]))
 	}
-	if len(manager.infoStreams) != 0 {
-		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 0, len(manager.infoStreams))
+	if len(manager.streams[Info]) != 0 {
+		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Info]))
 	}
-	if len(manager.warningStreams) != 0 {
-		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 0, len(manager.warningStreams))
+	if len(manager.streams[Warning]) != 0 {
+		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Warning]))
 	}
-	if len(manager.errorStreams) != 0 {
-		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 0, len(manager.errorStreams))
+	if len(manager.streams[Error]) != 0 {
+		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Error]))
 	}
-	if len(manager.fatalStreams) != 0 {
-		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.warningStreams))
+	if len(manager.streams[Fatal]) != 0 {
+		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Fatal]))
+	}
+	if len(manager.streams[Panic]) != 0 {
+		t.Fatalf("Unexpected panic stream len. Expected: %d - Found: %d.", 1, len(manager.streams[Panic]))
 	}
 
 	if err := manager.Register(Info, stream); err != nil {
 		t.Fatalf("Unexpected error: %s", err.Error())
 	}
-	if len(manager.debugStreams) != 1 {
-		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.debugStreams))
+	if len(manager.streams[Debug]) != 1 {
+		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Debug]))
 	}
-	if len(manager.verboseStreams) != 1 {
-		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.verboseStreams))
+	if len(manager.streams[Verbose]) != 1 {
+		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Verbose]))
 	}
-	if len(manager.infoStreams) != 1 {
-		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 1, len(manager.infoStreams))
+	if len(manager.streams[Info]) != 1 {
+		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Info]))
 	}
-	if len(manager.warningStreams) != 0 {
-		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 0, len(manager.warningStreams))
+	if len(manager.streams[Warning]) != 0 {
+		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Warning]))
 	}
-	if len(manager.errorStreams) != 0 {
-		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 0, len(manager.errorStreams))
+	if len(manager.streams[Error]) != 0 {
+		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Error]))
 	}
-	if len(manager.fatalStreams) != 0 {
-		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.warningStreams))
+	if len(manager.streams[Fatal]) != 0 {
+		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Fatal]))
+	}
+	if len(manager.streams[Panic]) != 0 {
+		t.Fatalf("Unexpected panic stream len. Expected: %d - Found: %d.", 1, len(manager.streams[Panic]))
 	}
 
 	if err := manager.Register(Warning, stream); err != nil {
 		t.Fatalf("Unexpected error: %s", err.Error())
 	}
-	if len(manager.debugStreams) != 1 {
-		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.debugStreams))
+	if len(manager.streams[Debug]) != 1 {
+		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Debug]))
 	}
-	if len(manager.verboseStreams) != 1 {
-		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.verboseStreams))
+	if len(manager.streams[Verbose]) != 1 {
+		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Verbose]))
 	}
-	if len(manager.infoStreams) != 1 {
-		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 1, len(manager.infoStreams))
+	if len(manager.streams[Info]) != 1 {
+		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Info]))
 	}
-	if len(manager.warningStreams) != 1 {
-		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 1, len(manager.warningStreams))
+	if len(manager.streams[Warning]) != 1 {
+		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Warning]))
 	}
-	if len(manager.errorStreams) != 0 {
-		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 0, len(manager.errorStreams))
+	if len(manager.streams[Error]) != 0 {
+		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Error]))
 	}
-	if len(manager.fatalStreams) != 0 {
-		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.warningStreams))
+	if len(manager.streams[Fatal]) != 0 {
+		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Fatal]))
+	}
+	if len(manager.streams[Panic]) != 0 {
+		t.Fatalf("Unexpected panic stream len. Expected: %d - Found: %d.", 1, len(manager.streams[Panic]))
 	}
 
 	if err := manager.Register(Error, stream); err != nil {
 		t.Fatalf("Unexpected error: %s", err.Error())
 	}
-	if len(manager.debugStreams) != 1 {
-		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.debugStreams))
+	if len(manager.streams[Debug]) != 1 {
+		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Debug]))
 	}
-	if len(manager.verboseStreams) != 1 {
-		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.verboseStreams))
+	if len(manager.streams[Verbose]) != 1 {
+		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Verbose]))
 	}
-	if len(manager.infoStreams) != 1 {
-		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 1, len(manager.infoStreams))
+	if len(manager.streams[Info]) != 1 {
+		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Info]))
 	}
-	if len(manager.warningStreams) != 1 {
-		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 1, len(manager.warningStreams))
+	if len(manager.streams[Warning]) != 1 {
+		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Warning]))
 	}
-	if len(manager.errorStreams) != 1 {
-		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 1, len(manager.errorStreams))
+	if len(manager.streams[Error]) != 1 {
+		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Error]))
 	}
-	if len(manager.fatalStreams) != 0 {
-		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.warningStreams))
+	if len(manager.streams[Fatal]) != 0 {
+		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 0, len(manager.streams[Fatal]))
 	}
-	
+	if len(manager.streams[Panic]) != 0 {
+		t.Fatalf("Unexpected panic stream len. Expected: %d - Found: %d.", 1, len(manager.streams[Panic]))
+	}
+
 	if err := manager.Register(Fatal, stream); err != nil {
 		t.Fatalf("Unexpected error: %s", err.Error())
 	}
-	if len(manager.debugStreams) != 1 {
-		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.debugStreams))
+	if len(manager.streams[Debug]) != 1 {
+		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Debug]))
 	}
-	if len(manager.verboseStreams) != 1 {
-		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.verboseStreams))
+	if len(manager.streams[Verbose]) != 1 {
+		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Verbose]))
 	}
-	if len(manager.infoStreams) != 1 {
-		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 1, len(manager.infoStreams))
+	if len(manager.streams[Info]) != 1 {
+		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Info]))
 	}
-	if len(manager.warningStreams) != 1 {
-		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 1, len(manager.warningStreams))
+	if len(manager.streams[Warning]) != 1 {
+		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Warning]))
 	}
-	if len(manager.errorStreams) != 1 {
-		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 1, len(manager.errorStreams))
+	if len(manager.streams[Error]) != 1 {
+		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Error]))
 	}
-	if len(manager.fatalStreams) != 1 {
-		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 1, len(manager.warningStreams))
+	if len(manager.streams[Fatal]) != 1 {
+		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Fatal]))
+	}
+	if len(manager.streams[Panic]) != 0 {
+		t.Fatalf("Unexpected panic stream len. Expected: %d - Found: %d.", 1, len(manager.streams[Panic]))
+	}
+
+	if err := manager.Register(Panic, stream); err != nil {
+		t.Fatalf("Unexpected error: %s", err.Error())
+	}
+	if len(manager.streams[Debug]) != 1 {
+		t.Fatalf("Unexpected debugStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Debug]))
+	}
+	if len(manager.streams[Verbose]) != 1 {
+		t.Fatalf("Unexpected verboseStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Verbose]))
+	}
+	if len(manager.streams[Info]) != 1 {
+		t.Fatalf("Unexpected infoStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Info]))
+	}
+	if len(manager.streams[Warning]) != 1 {
+		t.Fatalf("Unexpected warningStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Warning]))
+	}
+	if len(manager.streams[Error]) != 1 {
+		t.Fatalf("Unexpected errorStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Error]))
+	}
+	if len(manager.streams[Fatal]) != 1 {
+		t.Fatalf("Unexpected fatalStreams len. Expected: %d - Found: %d.", 1, len(manager.streams[Fatal]))
+	}
+	if len(manager.streams[Panic]) != 1 {
+		t.Fatalf("Unexpected panic stream len. Expected: %d - Found: %d.", 1, len(manager.streams[Panic]))
 	}
 }
 
@@ -159,7 +201,7 @@ func TestStreamManagerSend(t *testing.T) {
 	if err := manager.Send(LogLevel(9999), sampleMessage); err == nil {
 		t.Fatalf("Expected error for invalid log level. Found nil instead.")
 	}
-	
+
 	if err := manager.Register(Debug, stream); err != nil {
 		t.Fatalf("Unexpected error: %s", err.Error())
 	}

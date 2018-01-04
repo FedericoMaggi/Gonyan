fa13880ae106e5d8f77a1c0c3bd92ec9c3e83af6
Federico Maggi <federico.maggi@xtn-lab.com>
2018-01-04 23:40:38 +0100

    tests for stdout and stderr

--- logger_test.go
+++ logger_test.go
@@ -1,9 +1,24 @@
 package gonyan
 
-import "testing"
-import "os"
+import (
+	"os"
+	"testing"
+)
 
-func TestNewLogger(t *testing.T) {
-	l := NewLogger("X", nil, true)
+// TestNewLoggerWithStdout will test that the os.Stdout
+// file is compatible with current Stream implementation.
+// TODO: Capture stdout content to verify correct logging.
+func TestNewLoggerWithStdout(t *testing.T) {
+	l := NewLogger("X", nil, false)
 	l.RegisterStream(Debug, os.Stdout)
+	l.Debug("this is a debug log and should appear on stdout")
+}
+
+// TestNewLoggerWithStderr will test that the os.Stdout
+// file is compatible with current Stream implementation.
+// TODO: Capture stderr content to verify correct logging.
+func TestNewLoggerWithStderr(t *testing.T) {
+	l := NewLogger("X", nil, true)
+	l.RegisterStream(Error, os.Stderr)
+	l.Error("this is an error log")
 }

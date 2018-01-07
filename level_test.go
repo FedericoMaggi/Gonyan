333bdcd8b37c6d8c7515227017b2d677e39ef855
Federico Maggi <federico.maggi@xtn-lab.com>
2018-01-07 18:23:24 +0100

    Panic label test added.

--- level_test.go
+++ level_test.go
@@ -23,4 +23,7 @@ func TestGetLevelLabel(t *testing.T) {
 	if label := GetLevelLabel(Fatal); label != "Fatal" {
 		t.Fatalf("Invalid level label for Fatal level found: %s", label)
 	}
+	if label := GetLevelLabel(Panic); label != "Panic" {
+		t.Fatalf("Invalid level label for Panic level found: %s", label)
+	}
 }
